742d7fdce2b0ae5625080f1c02ef9cc39f8ceed0
annafavaro <anna.favaro96@gmail.com>
2024-02-04 15:08:57 -0500

    .

--- feature_extraction/compare_features.py
+++ feature_extraction/compare_features.py
@@ -13,13 +13,15 @@ smile = opensmile.Smile(
 )
 
 dfs = []  # List to store DataFrames extracted from each audio file
-
+names = []
 for file in all_files:
+    names.append(os.path.basename(file))
     print(file)
     y = smile.process_file(file)
     dfs.append(pd.DataFrame(y))
 
 # Concatenate all DataFrames into a single DataFrame
 concatenated_df = pd.concat(dfs, ignore_index=True)
+concatenated_df['ID'] = names
 out_file = os.path.join(out_dir, 'compare_features.csv')
 concatenated_df.to_csv(out_file)
