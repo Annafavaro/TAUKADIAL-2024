f66e6cdb77ecc656adac626c936b9d89d42f8055
annafavaro <anna.favaro96@gmail.com>
2024-02-07 07:11:30 -0500

    .

--- pre_processing/from_wav_to_ogg.py
+++ pre_processing/from_wav_to_ogg.py
@@ -4,32 +4,32 @@ import subprocess
 out = '/export/b01/afavaro/INTERSPEECH_2024/TAUKADIAL-24/training/train_audios_16k_no_diatization_ogg/'
 input_dir = '/export/b01/afavaro/INTERSPEECH_2024/TAUKADIAL-24/training/train_audios_original/'
 
-#def convert_wav_to_ogg(input_file, output_file):
-#    # Load the WAV file
-#    sound = AudioSegment.from_wav(input_file)
-#    sound = sound.set_frame_rate(16000)
-#    # Export the audio in OGG format
-#    sound.export(output_file, format="ogg", codec="opus")
-
-
-
 def convert_wav_to_ogg(input_file, output_file):
-    # Define the FFmpeg command
-    ffmpeg_cmd = [
-        "ffmpeg",
-        "-i", input_file,
-        "-ar", "16000",  # Resample to 16 kHz
-        "-ac", "2",  # Set stereo channels
-        output_file
-    ]
+    # Load the WAV file
+    sound = AudioSegment.from_wav(input_file)
+    sound = sound.set_frame_rate(16000)
+    # Export the audio in OGG format
+    sound.export(output_file, format="ogg", codec="opus")
 
-    # Execute the FFmpeg command
-    try:
-        subprocess.run(ffmpeg_cmd, check=True)
-        print("Conversion successful!")
-    except subprocess.CalledProcessError as e:
-        print("Conversion failed:", e)
 
+
+#def convert_wav_to_ogg(input_file, output_file):
+#    # Define the FFmpeg command
+#    ffmpeg_cmd = [
+#        "ffmpeg",
+#        "-i", input_file,
+#        "-ar", "16000",  # Resample to 16 kHz
+#        "-ac", "2",  # Set stereo channels
+#        output_file
+#    ]
+#
+#    # Execute the FFmpeg command
+#    try:
+#        subprocess.run(ffmpeg_cmd, check=True)
+#        print("Conversion successful!")
+#    except subprocess.CalledProcessError as e:
+#        print("Conversion failed:", e)
+#
 
 
 all_audios = [os.path.join(input_dir, elem) for elem in os.listdir(input_dir)]
