018db2c970ec1b8cb0c6382d0a30b0584cd96f0c
annafavaro <anna.favaro96@gmail.com>
2024-02-07 07:00:13 -0500

    .

---
+++ pre_processing/from_wav_to_ogg.py
@@ -0,0 +1,18 @@
+from pydub import AudioSegment
+import os
+
+out = '/export/b01/afavaro/INTERSPEECH_2024/TAUKADIAL-24/training/train_audios_16k_no_diatization_ogg/'
+input_dir = '/export/b01/afavaro/INTERSPEECH_2024/TAUKADIAL-24/training/train_audios_16k_no_diarization/'
+
+def convert_wav_to_ogg(input_file, output_file):
+    # Load the WAV file
+    sound = AudioSegment.from_wav(input_file)
+
+    # Export the audio in OGG format
+    sound.export(output_file, format="ogg", codec="libvorbis")
+
+all_audios = [os.path.join(input_dir, elem) for elem in os.listdir(input_dir)]
+for audio in all_audios:
+    base = os.path.basename(audio)
+    out_file = os.path.join(out, base)
+    convert_wav_to_ogg(audio, out_file)
