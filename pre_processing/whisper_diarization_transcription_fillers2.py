bf2fcfb70b20d9b9ac238a5f5ad8d8d0ccfad705
annafavaro <anna.favaro96@gmail.com>
2024-02-07 12:01:28 -0500

    .

--- pre_processing/whisper_diarization_transcription_fillers2.py
+++ pre_processing/whisper_diarization_transcription_fillers2.py
@@ -4,7 +4,8 @@ root2 = '/scratch4/lmorove1/afavaro/data/TAUKADIAL-24/TAUKADIAL-24/train/'
 from openai import OpenAI  # for making OpenAI API calls
 import urllib  # for downloading example audio files
 import os
-token = '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'
+token = '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'
+#token = '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'
 #token = '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'
 #token = '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'
 #token= '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'
