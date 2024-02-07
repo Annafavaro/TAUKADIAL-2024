c68b6959b2257e7fd842c6a25f55304519fd2eca
annafavaro <anna.favaro96@gmail.com>
2024-02-07 16:15:29 -0500

    .

--- classification_experiments/prediction/non_interpretable/classification_balanced_average.py
+++ classification_experiments/prediction/non_interpretable/classification_balanced_average.py
@@ -124,25 +124,7 @@ for feat_name in feats_names:
         data.append(cn_sp + pd_sp)
     n_folds = sorted(data, key=len, reverse=True)
     #print(n_folds)
-#
-    # PER SPEAKER
-    #folds = []
-    #for fold in n_folds:
-    #    names_all = []
-    #    data_fold = np.array(())  # %
-    #    data_i = df_pd[df_pd["names"].isin(fold)]
-    #    names = list(set(data_i['names'].tolist()))
-    #    for name in names:
-    #        names_all.append(name)
-    #        gr_sp = data_i.groupby('names').get_group(name)
-    #        label_row = (gr_sp['labels'].tolist())[0]
-    #        read_vect = [np.load(vec) for vec in gr_sp['path_feat'].tolist()]
-    #        read_vect = [vec / np.linalg.norm(vec) for vec in read_vect]
-    #        feat = np.mean(read_vect, axis=0)
-    #        feat = feat / np.linalg.norm(feat)
-    #        feat = np.append(feat, label_row)
-    #        data_fold = np.vstack((data_fold, feat)) if data_fold.size else feat
-    #    folds.append(data_fold)
+
 #
     ## PER FILE
     folds = []
