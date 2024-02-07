061268c925d51cbf8cf0b354f911f423d5b45aa7
annafavaro <anna.favaro96@gmail.com>
2024-02-07 07:54:10 -0500

    .

--- pre_processing/whisper_diarization_transcription_fillers2.py
+++ pre_processing/whisper_diarization_transcription_fillers2.py
@@ -4,8 +4,10 @@ root2 = '/scratch4/lmorove1/afavaro/data/TAUKADIAL-24/TAUKADIAL-24/train/'
 from openai import OpenAI  # for making OpenAI API calls
 import urllib  # for downloading example audio files
 import os
-token= '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'
+token = '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'
+#token= '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'
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY", token))
+limit_mb=25
 
 # define a wrapper function for seeing how prompts affect transcriptions
 def transcribe(audio_filepath, prompt: str) -> str:
@@ -29,10 +31,13 @@ ids = []
 #index = all_files_audio.index(os.path.join(root2, 'taukdial-161-3.wav'))
 for audio_file in all_files_audio[150:]:
         print(audio_file)
-        base_name = os.path.basename(audio_file).split(".wav")[0]
-        OUT_PATH_FILE = os.path.join(OUT_PATH, base_name + '.txt')
-        transcript = transcribe(audio_file,
-                   prompt="Well, um, I was just, you know, walking into the kitchen, and, uh, I noticed that the cookie jar was, um, mysteriously open, and, like, there were crumbs all over the counter counter, so, ah, I think someone might might have, you know, helped themselves to a few cookies when, uh, nobody was around.")
-        with open(OUT_PATH_FILE,'w') as output:
-            for line in transcript:
-                output.write(line)
+        file_size_bytes = os.path.getsize(audio_file)
+        file_size_mb = file_size_bytes / (1024 * 1024)
+        if file_size_mb <= limit_mb:
+            base_name = os.path.basename(audio_file).split(".wav")[0]
+            OUT_PATH_FILE = os.path.join(OUT_PATH, base_name + '.txt')
+            transcript = transcribe(audio_file,
+                                    prompt="Well, um, I was just, you know, walking into the kitchen, and, uh, I noticed that the cookie jar was, um, mysteriously open, and, like, there were crumbs all over the counter counter, so, ah, I think someone might might have, you know, helped themselves to a few cookies when, uh, nobody was around.")
+            with open(OUT_PATH_FILE,'w') as output:
+                for line in transcript:
+                    output.write(line)
