2b494ef5d8707864d3103d86534cdd0e4436a795
annafavaro <anna.favaro96@gmail.com>
2024-02-14 10:06:58 -0500

    .

--- feature_extraction/linguistic/xlm-roberta-large.py
+++ feature_extraction/linguistic/xlm-roberta-large.py
@@ -3,28 +3,31 @@
 from sentence_transformers import SentenceTransformer
 import sys
 import os
-from transformers import AutoTokenizer, AutoModelForMaskedLM
-import torch.nn.functional as F
+from numpy import save
 from torch import Tensor
 from transformers import AutoTokenizer, AutoModel
 
-
-def average_pool(last_hidden_states: Tensor,
-                 attention_mask: Tensor) -> Tensor:
-    last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
-    return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
-
 # YES:
 # LANGUAGES: ENGLISH AND CHINESE, AMONG OTHERS
-#if __name__ == "__main__":
+if __name__ == "__main__":
 
-   # input_dir = sys.argv[1] # path to transcripts
-   # output_dir = sys.argv[2]
-model = SentenceTransformer('xlm-roberta-large')
-sentences = ["This is an example sentence"]
-embeddings = model.encode(sentences)
-print(embeddings)
-print(embeddings.shape)
+    input_dir = sys.argv[1] # path to transcripts
+    output_dir = sys.argv[2]
+    model = SentenceTransformer('xlm-roberta-large')
+    all_sents = sorted([os.path.join(input_dir, elem) for elem in os.listdir(input_dir)])
+    for sentences in all_sents:
+       base_name = os.path.basename(sentences).split(".txt")[0]
+       print(base_name)
+       # sentences = open(sentences, 'r', encoding="utf-8",errors='ignore').read().strip().lower()
+       with open(sentences, 'r', encoding="utf-8", errors='ignore') as file:
+           sentences = file.read().strip()#.lower()
+           sentences = [sentences]
+           embeddings = model.encode(sentences)
+           print(type(embeddings))
+           print(embeddings.shape)
+           print(type(embeddings))
+           save(output_dir + base_name + '.npy', embeddings)
+
 
 
 
