5b8b4745ee9baa42652e8e934b336fd623a312c8
annafavaro <anna.favaro96@gmail.com>
2024-02-14 08:12:56 -0500

    .

--- feature_extraction/linguistic/xlm-roberta-large.py
+++ feature_extraction/linguistic/xlm-roberta-large.py
@@ -8,6 +8,12 @@ import re
 import torch
 from numpy import save
 
+def mean_pooling(model_output, attention_mask):
+    token_embeddings = model_output[0] #First element of model_output contains all token embeddings
+    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+
+
 # YES:
 # LANGUAGES: ENGLISH AND CHINESE, AMONG OTHERS
 if __name__ == "__main__":
@@ -29,9 +35,10 @@ if __name__ == "__main__":
             with torch.no_grad():
                 output = model(**encoded_input)#['logits']
 
-            print(type(output))
-            #print(output.shape)
-            print(output.keys())
+            embeddings = mean_pooling(output, encoded_input['attention_mask'])
+            print(type(embeddings))
+            print(output.shape)
+            #print(output.keys())
             #embeddings = model.encode(sentences)
            # embeddings = embeddings.reshape(1, -1)
             #print(type(embeddings))
