93c0b2d47f3913e638471980333fc57740f6a935
annafavaro <anna.favaro96@gmail.com>
2024-02-14 07:06:34 -0500

    .

--- feature_extraction/linguistic/bert-base-multilingual-cased.py
+++ feature_extraction/linguistic/bert-base-multilingual-cased.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
             encoded_input = tokenizer(sentences, return_tensors='pt')
             output = model(**encoded_input)
             embeddings = output['last_hidden_state']
-            print(type(output))
-            print(output.shape)
-           # embeddings = output.numpy()
+            print(type(embeddings))
+            print(embeddings.shape)
+           # embeddings = embeddings.numpy()
            # save(output_dir + base_name + '.npy', embeddings)
