26c17d09668816ff2d86bbaada3fe5a39b090965
annafavaro <anna.favaro96@gmail.com>
2024-02-14 09:31:02 -0500

    .

--- feature_extraction/linguistic/xlm-roberta-large.py
+++ feature_extraction/linguistic/xlm-roberta-large.py
@@ -20,9 +20,11 @@ if __name__ == "__main__":
 
     input_dir = sys.argv[1] # path to transcripts
     output_dir = sys.argv[2]
-    model = SentenceTransformer('FacebookAI/xlm-roberta-large')
+    model = SentenceTransformer('sentence-transformers/xlm-roberta-large')
+    sentences = ["This is an example sentence"]
     embeddings = model.encode(sentences)
     print(embeddings)
+    print(embeddings.shape)
 
 
 
@@ -34,27 +36,26 @@ if __name__ == "__main__":
 
 
 
+   #tokenizer = AutoTokenizer.from_pretrained('xlm-roberta-large')
+   #model = AutoModelForMaskedLM.from_pretrained("xlm-roberta-large")
 
-    tokenizer = AutoTokenizer.from_pretrained('xlm-roberta-large')
-    model = AutoModelForMaskedLM.from_pretrained("xlm-roberta-large")
+   #all_sents = sorted([os.path.join(input_dir, elem) for elem in os.listdir(input_dir)])
+   #for sentences in all_sents:
+   #    base_name = os.path.basename(sentences).split(".txt")[0]
+   #    print(base_name)
+   #    # sentences = open(sentences, 'r', encoding="utf-8",errors='ignore').read().strip().lower()
+   #    with open(sentences, 'r', encoding="utf-8", errors='ignore') as file:
+   #        sentences = file.read().strip()#.lower()
+   #        # Tokenize sentences
+   #        batch_dict = tokenizer(sentences, max_length=512, padding=True, truncation=True, return_tensors='pt')
+   #        outputs = model(**batch_dict)
+   #        print(outputs.keys())
+   #        embeddings = average_pool(outputs.logits, batch_dict['attention_mask'])
 
-    all_sents = sorted([os.path.join(input_dir, elem) for elem in os.listdir(input_dir)])
-    for sentences in all_sents:
-        base_name = os.path.basename(sentences).split(".txt")[0]
-        print(base_name)
-        # sentences = open(sentences, 'r', encoding="utf-8",errors='ignore').read().strip().lower()
-        with open(sentences, 'r', encoding="utf-8", errors='ignore') as file:
-            sentences = file.read().strip()#.lower()
-            # Tokenize sentences
-            batch_dict = tokenizer(sentences, max_length=512, padding=True, truncation=True, return_tensors='pt')
-            outputs = model(**batch_dict)
-            print(outputs.keys())
-            embeddings = average_pool(outputs.logits, batch_dict['attention_mask'])
+   #        ## normalize embeddings
+   #        embeddings = F.normalize(embeddings, p=2, dim=1)
+   #        embeddings = embeddings.detach().numpy()
+   #        print(type(embeddings))
+   #        print(embeddings.shape)
 
-            ## normalize embeddings
-            embeddings = F.normalize(embeddings, p=2, dim=1)
-            embeddings = embeddings.detach().numpy()
-            print(type(embeddings))
-            print(embeddings.shape)
-
-            #save(output_dir + base_name + '.npy', embeddings)
+   #        #save(output_dir + base_name + '.npy', embeddings)
