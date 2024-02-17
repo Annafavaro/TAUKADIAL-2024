8f3eb98043c70bed690299459cd9cb217cf5d7e6
annafavaro <anna.favaro96@gmail.com>
2024-02-17 08:27:42 -0500

    .

--- feature_extraction/acoustic/compare_features.py
+++ feature_extraction/acoustic/compare_features.py
@@ -1,27 +1,31 @@
+#conda activate opensmile
+
 import opensmile
 import pandas as pd
 import os
-#conda activate opensmile
+import sys
 
-input_dir = '/export/b01/afavaro/INTERSPEECH_2024/TAUKADIAL-24/training/train_audios_16k_no_diarization/'
-out_dir = '/export/b01/afavaro/INTERSPEECH_2024/TAUKADIAL-24/training/feats/compare/no_diarization/'
+if __name__ == "__main__":
 
-all_files = [os.path.join(input_dir, elem) for elem in os.listdir(input_dir)]
-smile = opensmile.Smile(
-    feature_set=opensmile.FeatureSet.ComParE_2016,
-    feature_level=opensmile.FeatureLevel.Functionals,
-)
+    input_dir = sys.argv[1]
+    out_dir = sys.argv[2]
 
-dfs = []  # List to store DataFrames extracted from each audio file
-names = []
-for file in all_files:
-    names.append(os.path.basename(file))
-    print(file)
-    y = smile.process_file(file)
-    dfs.append(pd.DataFrame(y))
+    all_files = [os.path.join(input_dir, elem) for elem in os.listdir(input_dir)]
+    smile = opensmile.Smile(
+        feature_set=opensmile.FeatureSet.ComParE_2016,
+        feature_level=opensmile.FeatureLevel.Functionals,
+    )
 
-# Concatenate all DataFrames into a single DataFrame
-concatenated_df = pd.concat(dfs, ignore_index=True)
-concatenated_df['ID'] = names
-out_file = os.path.join(out_dir, 'compare_features.csv')
-concatenated_df.to_csv(out_file)
+    dfs = []  # List to store DataFrames extracted from each audio file
+    names = []
+    for file in all_files:
+        names.append(os.path.basename(file))
+        print(file)
+        y = smile.process_file(file)
+        dfs.append(pd.DataFrame(y))
+
+    # Concatenate all DataFrames into a single DataFrame
+    concatenated_df = pd.concat(dfs, ignore_index=True)
+    concatenated_df['ID'] = names
+    out_file = os.path.join(out_dir, 'compare_features.csv')
+    concatenated_df.to_csv(out_file)
