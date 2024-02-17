b1ee3c9292ee18b3035b5a1f5914c56a7756a09d
annafavaro <anna.favaro96@gmail.com>
2024-02-17 12:52:06 -0500

    .

--- finetuning/all/all_1.py
+++ finetuning/all/all_1.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     def preprocess_function(examples):
         if sentence2_key is None:
             return tokenizer(examples[sentence1_key], truncation=True, padding=True)
-        return tokenizer(examples[sentence1_key], examples[sentence2_key], truncation=True, padding=True, max_length=560,
+        return tokenizer(examples[sentence1_key], examples[sentence2_key], truncation=True, padding=True, max_length=256,
         return_tensors="pt")
 
     def compute_metrics(pred):
