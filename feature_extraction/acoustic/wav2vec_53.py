fb6281b930d8cf409beab3ada9b6c3608111f62c
annafavaro <anna.favaro96@gmail.com>
2024-02-17 18:19:57 -0500

    .

--- feature_extraction/acoustic/wav2vec_53.py
+++ feature_extraction/acoustic/wav2vec_53.py
@@ -4,10 +4,7 @@ import os
 from numpy import save
 from transformers import Wav2Vec2FeatureExtractor, Wav2Vec2Model
 import librosa
-
-path_audios = '/export/b01/afavaro/INTERSPEECH_2024/TAUKADIAL-24/training/train_audios_16k_no_diarization/'
-all_audios = [os.path.join(path_audios, elem) for elem in os.listdir(path_audios)]
-output_path = '/export/b01/afavaro/INTERSPEECH_2024/TAUKADIAL-24/training/feats/embeddings/wav2vec/'
+import sys
 
 
 def get_all_sub_segment_inds(x, fs=16e3, dur=10):
@@ -46,26 +43,36 @@ def get_sub_segment(x, fs=16e3, dur=10, index=0):
 
     return seg
 
-model_name = "facebook/wav2vec2-large-xlsr-53"
-feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(model_name)
-model = Wav2Vec2Model.from_pretrained(model_name)
 
-for audio in all_audios[70:]:
-    print(audio)
-    base = os.path.basename(audio).split('.wav')[0]
-    x, fs = librosa.load(audio, sr=16000)
-    x = x / (max(abs(x)))
-    ind_range = get_all_sub_segment_inds(x, fs=16e3, dur=10) # 10sec segments
-    embeddings = np.zeros(shape=(ind_range, 512))
 
-    for spec_ind in range(ind_range):
-        seg = get_sub_segment(x, fs=16e3, dur=10, index=spec_ind)
-        inputs = feature_extractor(seg, sampling_rate=16000, padding=True, return_tensors="pt")
-        hidden_states = model(inputs.input_values)
-        embeddings[spec_ind,:] = hidden_states['extract_features'].squeeze().mean(dim=0).detach().numpy()
-        # This gives you the sequence of features from the last convolutional layer of the model.
 
-    hidden_layer_avg = np.mean(embeddings, axis=0)
-    hidden_layer_avg = hidden_layer_avg.reshape((1, 512))
-    out_path = os.path.join(output_path, base + '.npy')
-    save(out_path, hidden_layer_avg)
+if __name__ == "__main__":
+
+    path_audios = sys.argv[1]
+    text_dir = sys.argv[2]
+    output_path = sys.argv[3]
+
+    all_audios = [os.path.join(path_audios, elem) for elem in os.listdir(path_audios)]
+    model_name = "facebook/wav2vec2-large-xlsr-53"
+    feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(model_name)
+    model = Wav2Vec2Model.from_pretrained(model_name)
+
+    for audio in all_audios:
+        print(audio)
+        base = os.path.basename(audio).split('.wav')[0]
+        x, fs = librosa.load(audio, sr=16000)
+        x = x / (max(abs(x)))
+        ind_range = get_all_sub_segment_inds(x, fs=16e3, dur=10) # 10sec segments
+        embeddings = np.zeros(shape=(ind_range, 512))
+
+        for spec_ind in range(ind_range):
+            seg = get_sub_segment(x, fs=16e3, dur=10, index=spec_ind)
+            inputs = feature_extractor(seg, sampling_rate=16000, padding=True, return_tensors="pt")
+            hidden_states = model(inputs.input_values)
+            embeddings[spec_ind,:] = hidden_states['extract_features'].squeeze().mean(dim=0).detach().numpy()
+            # This gives you the sequence of features from the last convolutional layer of the model.
+
+        hidden_layer_avg = np.mean(embeddings, axis=0)
+        hidden_layer_avg = hidden_layer_avg.reshape((1, 512))
+        out_path = os.path.join(output_path, base + '.npy')
+        save(out_path, hidden_layer_avg)
