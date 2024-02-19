6b25a4642d06d0a53c86536469bb7bdd5dead4e1
annafavaro <anna.favaro96@gmail.com>
2024-02-18 19:20:21 -0500

    .

--- classification_experiments/regression/language_wise/non_interpretable/MMSE_normal_loss_all.py
+++ classification_experiments/regression/language_wise/non_interpretable/MMSE_normal_loss_all.py
@@ -28,15 +28,30 @@ torch.manual_seed(seed)
 
 
 # Define a custom neural network model
+#class MMSE_ModelBasic(nn.Module):
+#    def __init__(self, input_size, hidden_size):
+#        super(MMSE_ModelBasic, self).__init__()
+#        self.fc1 = nn.Linear(input_size, hidden_size)
+#        self.fc2 = nn.Linear(hidden_size, 1)
+#
+#    def forward(self, x):
+#        x = torch.relu(self.fc1(x))
+#        x = self.fc2(x)
+#        return x
+#
+
+
 class MMSE_ModelBasic(nn.Module):
     def __init__(self, input_size, hidden_size):
         super(MMSE_ModelBasic, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, 1)
+        self.fc2 = nn.Linear(hidden_size, 20)
+        self.fc3 = nn.Linear(20, 1)
 
     def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
 
 
