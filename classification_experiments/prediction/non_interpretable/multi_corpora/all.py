52b390b34b8d241359a1185fa5412a0820fb5733
annafavaro <anna.favaro96@gmail.com>
2024-02-19 06:53:24 -0500

    .

--- classification_experiments/prediction/non_interpretable/multi_corpora/all.py
+++ classification_experiments/prediction/non_interpretable/multi_corpora/all.py
@@ -534,7 +534,7 @@ for feat_name in feats_names:
                                normalized_train_lu, normalized_train_del, normalized_train_adr,
                                 ], axis=0)
 
-        y_train = np.concatenate([y_train_en, y_train_zh, normalized_train_lu, normalized_train_del, normalized_train_adr], axis=0)
+        y_train = np.concatenate([y_train_en, y_train_zh, y_train_lu, y_train_del, y_train_adr], axis=0)
 
         #Xtrain = np.concatenate([normalized_train_en,
                               #   normalized_train_lu, normalized_train_del, normalized_train_adr,
