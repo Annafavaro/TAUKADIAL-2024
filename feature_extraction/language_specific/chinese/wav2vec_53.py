4680f7982fd5131b5a74a2e2449e69c7176053e4
annafavaro <anna.favaro96@gmail.com>
2024-02-19 20:00:42 -0500

    .

--- feature_extraction/language_specific/chinese/wav2vec_53.py
+++ feature_extraction/language_specific/chinese/wav2vec_53.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     output_path = sys.argv[2]
 
     all_audios = [os.path.join(path_audios, elem) for elem in os.listdir(path_audios)]
-    model_name = "facebook/wav2vec2-large-xlsr-53"
+    model_name = 'wcfr/wav2vec2-conformer-rel-pos-base-cantonese'
     feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(model_name)
     model = Wav2Vec2Model.from_pretrained(model_name)
 
@@ -73,4 +73,4 @@ if __name__ == "__main__":
         hidden_layer_avg = hidden_layer_avg.reshape((1, 512))
         out_path = os.path.join(output_path, base + '.npy')
         print(hidden_layer_avg.shape)
-        #save(out_path, hidden_layer_avg)
+        save(out_path, hidden_layer_avg)
