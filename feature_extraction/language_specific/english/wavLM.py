b5b962cf93a70e564616c78069b990b704e6d62a
annafavaro <anna.favaro96@gmail.com>
2024-02-20 09:07:13 -0500

    .

---
+++ feature_extraction/language_specific/english/wavLM.py
@@ -0,0 +1,26 @@
+from transformers import AutoFeatureExtractor, WavLMForXVector
+from datasets import load_dataset
+import torch
+import sys
+import os
+import librosa
+
+if __name__ == "__main__":
+
+    input_dir = sys.argv[1] # path to transcripts
+    output_dir = sys.argv[2]
+
+    model_name = "microsoft/wavlm-base-plus-sv"
+
+    feature_extractor = AutoFeatureExtractor.from_pretrained("microsoft/wavlm-base-plus-sv")
+    model = WavLMForXVector.from_pretrained("microsoft/wavlm-base-plus-sv")
+    all_audios = [os.path.join(input_dir, elem) for elem in os.listdir(input_dir)]
+
+    for audio in all_audios:
+        print(audio)
+        base = os.path.basename(audio).split('.wav')[0]
+        inputs, fs = librosa.load(audio, sr=16000)
+        with torch.no_grad():
+            embeddings = model(**inputs).embeddings
+        embeddings = torch.nn.functional.normalize(embeddings, dim=-1).cpu()
+
