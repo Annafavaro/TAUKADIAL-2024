29f03f90e8f3afe06560d21feb41b0333d79acb7
annafavaro <anna.favaro96@gmail.com>
2024-02-21 08:19:23 -0500

    .

--- classification_experiments/prediction/non_interpretable/multi_corpora/PLDA/english_multi.py
+++ classification_experiments/prediction/non_interpretable/multi_corpora/PLDA/english_multi.py
@@ -131,10 +131,7 @@ for feat_name in feats_names:
         name = os.path.basename(file).split('_ses')[0]
         print(name)
         if name in names_to_keep:
-            if name in names_to_keep_ad:
-                label_row = 0
-            else:
-                label_row = 1
+            label_row = [0 if label_row in names_to_keep_ad else 1]
             print(label_row)
             feat = np.load(file)
             feat = np.append(feat, label_row)
