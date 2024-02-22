5c3ed0967c4d5058a5fd350759e40de37255e3c2
annafavaro <anna.favaro96@gmail.com>
2024-02-22 15:29:31 -0500

    .

--- finetuning/mono/english/bert_finetuning_4.py
+++ finetuning/mono/english/bert_finetuning_4.py
@@ -1,4 +1,4 @@
-cv_num = 5
+cv_num = 4
 out_scores = '/export/b01/afavaro/INTERSPEECH_2024/TAUKADIAL-24/training/finetuning/results/mono/english/'
 finetuning_data = f'/export/b01/afavaro/INTERSPEECH_2024/TAUKADIAL-24/training/finetuning/data/mono/english/cv_{cv_num}/'
 
