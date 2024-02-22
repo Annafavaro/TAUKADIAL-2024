5c5b4bd54e25d8d80829a2495b052b02b0d14a17
annafavaro <anna.favaro96@gmail.com>
2024-02-22 09:11:53 -0500

    .

--- classification_experiments/prediction/non_interpretable/language_w_aug/DNN/language_wise_english.py
+++ classification_experiments/prediction/non_interpretable/language_w_aug/DNN/language_wise_english.py
@@ -118,7 +118,7 @@ for feat_name in feats_names:
             label_row = speaker[-2]
             mmse = speaker[-1]
             if feat_name ==  'trillsson' or feat_name== 'xvector' or feat_name == 'wav2vec_128' or feat_name == 'wav2vec_53':
-                all_copies = [0, 1, 2]
+                all_copies = [0,  2, 4]
             else:
                 all_copies = [0, 1, 6]#np.arange(0, 7)
            # print(all_copies, feat_name)
