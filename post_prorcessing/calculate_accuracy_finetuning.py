77264b1ddb827ab2c740ab323a01d0d8269d486f
annafavaro <anna.favaro96@gmail.com>
2024-02-23 13:46:10 -0500

    .

---
+++ post_prorcessing/calculate_accuracy_finetuning.py
@@ -0,0 +1,18 @@
+import numpy as np
+import os
+import pandas as pd
+#a = np.array([0,0,1,1,1])   # actual labels
+#b = np.array([1,1,0,0,1])   # predicted labels
+#
+#correct = (a == b)
+#accuracy = correct.sum() / correct.size
+
+list_results = '/export/b01/afavaro/INTERSPEECH_2024/TAUKADIAL-24/training/finetuning/results/mono/chinese/'
+
+all_res = [os.path.join(list_results, elem)  for elem in os.listdir(list_results)]
+
+all_accs = []
+for data in all_res:
+    all_accs.append(['accuracy'].tolist())
+
+print(all_accs/len(all_accs))
