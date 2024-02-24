a7f8ac74d11378f68c3b42a49d2dcbe70cfc47aa
annafavaro <anna.favaro96@gmail.com>
2024-02-24 13:47:13 -0500

    .

--- classification_experiments/prediction/non_interpretable/multi_corpora/DNN/zh_multi.py
+++ classification_experiments/prediction/non_interpretable/multi_corpora/DNN/zh_multi.py
@@ -319,7 +319,7 @@ for feat_name in feats_names:
     ####################################################################
 
     n_epochs = 30
-    batch_size = 48
+    batch_size = 32
     # hidden_dim = 40  # Hidden dimension of the fully connected layer
     output_dim = 1  # Output dimension for binary classification (1 for binary)
     learning_rate = 0.001
