3cc026282e212c642e326a451f59d1a3a720038b
annafavaro <anna.favaro96@gmail.com>
2024-02-24 14:12:45 -0500

    .

--- classification_experiments/regression/language_wise/non_interpretable/MMSE_normal_loss_en.py
+++ classification_experiments/regression/language_wise/non_interpretable/MMSE_normal_loss_en.py
@@ -189,7 +189,7 @@ for feat_name in feats_names:
     data_test_9_names = np.concatenate(n_folds_names[7:8])
     data_test_10_names = np.concatenate(n_folds_names[8:9])
 
-    learning_rate = 0.1
+    learning_rate = 0.01
     num_epochs = 300
     batch_size = 64
     input_size = data_train_1.shape[1] - 2
