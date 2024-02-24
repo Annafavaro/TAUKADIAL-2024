1ad6b4c72e79c116ebd6f07d9908174d2fa5aeb1
annafavaro <anna.favaro96@gmail.com>
2024-02-24 14:10:11 -0500

    .

--- classification_experiments/regression/language_wise/non_interpretable/MMSE_normal_loss_en.py
+++ classification_experiments/regression/language_wise/non_interpretable/MMSE_normal_loss_en.py
@@ -191,7 +191,7 @@ for feat_name in feats_names:
 
     learning_rate = 0.01
     num_epochs = 300
-    batch_size = 48
+    batch_size = 64
     input_size = data_train_1.shape[1] - 2
     hidden_size = 40
     criterion = nn.MSELoss()
