6888e792aca95072b32063b9aae25fde32506f62
annafavaro <anna.favaro96@gmail.com>
2024-02-25 15:18:08 -0500

    .

--- finetuning/bert/mono/english/bert_finetuning_all.py
+++ finetuning/bert/mono/english/bert_finetuning_all.py
@@ -1,5 +1,6 @@
 
 import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 from datasets import Dataset, DatasetDict
 from datasets import Dataset
 import pandas as pd
@@ -13,7 +14,6 @@ from datasets import load_metric
 import torch
 torch.manual_seed(40)
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 #checkpoint = 'distilbert-base-cased'
 checkpoint='bert-base-cased'
