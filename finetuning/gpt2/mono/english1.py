c1d09df1a2937f8c108b1564c0b0e6c8ef20e21b
annafavaro <anna.favaro96@gmail.com>
2024-02-24 21:10:30 -0500

    .

--- finetuning/gpt2/mono/english1.py
+++ finetuning/gpt2/mono/english1.py
@@ -85,8 +85,8 @@ callbacks = tf.keras.callbacks.EarlyStopping(
 y_train_in = tf.constant(y_train, dtype=tf.int32)
 y_test_in = tf.constant(y_test, dtype=tf.int32)
 tf.config.experimental_run_functions_eagerly(True)
-#history = clf.fit([X_train_in, X_train_mask], y_train_in, epochs=1, batch_size=32, validation_split=0.2,
-                #  callbacks=callbacks)
+history = clf.fit([X_train_in, X_train_mask], y_train_in, epochs=1, batch_size=32, validation_split=0.2,
+                  callbacks=callbacks)
 
 clf.evaluate([X_test_in, X_test_mask], y_test_in)
 clf.training = False
