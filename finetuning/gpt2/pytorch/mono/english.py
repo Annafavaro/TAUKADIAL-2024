c29395e6db841040cf6565a770a52a24d0784c60
annafavaro <anna.favaro96@gmail.com>
2024-02-25 10:03:07 -0500

    .

--- finetuning/gpt2/pytorch/mono/english.py
+++ finetuning/gpt2/pytorch/mono/english.py
@@ -405,8 +405,8 @@ for cv_num in cv_range:
     # Note: AdamW is a class from the huggingface library (as opposed to pytorch)
     # I believe the 'W' stands for 'Weight Decay fix"
     optimizer = AdamW(model.parameters(),
-                      lr = 2e-4, # default is 5e-5, our notebook had 2e-5
-                      eps = 1e-8 # default is 1e-8.
+                      lr = 2e-5, # default is 5e-5, our notebook had 2e-5
+                      eps = 1e-9 # default is 1e-8.
                       )
 
     total_steps = len(train_dataloader) * epochs
