fa388cd773ee18873dbbe6fbd45e8cd8f4e5da6e
annafavaro <anna.favaro96@gmail.com>
2024-02-25 09:29:35 -0500

    .

--- post_prorcessing/calculate_accuracy_finetuning.py
+++ post_prorcessing/calculate_accuracy_finetuning.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 #list_results = '/export/b01/afavaro/INTERSPEECH_2024/TAUKADIAL-24/training/finetuning/results/mono/chinese/'
-list_results = '/export/b01/afavaro/INTERSPEECH_2024/TAUKADIAL-24/training/finetuning/results/multi/chinese/'
+list_results = '/export/b01/afavaro/INTERSPEECH_2024/TAUKADIAL-24/training/finetuning/results/chatgpt_pytorch/mono/english/'
 all_res = [os.path.join(list_results, elem)  for elem in os.listdir(list_results)]
 
 all_accs = []
