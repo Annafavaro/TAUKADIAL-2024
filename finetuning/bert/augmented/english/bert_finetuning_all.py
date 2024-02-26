81267481235401760c1d14af89f38cefabf404b7
annafavaro <anna.favaro96@gmail.com>
2024-02-26 06:13:07 -0500

    .

--- finetuning/bert/augmented/english/bert_finetuning_all.py
+++ finetuning/bert/augmented/english/bert_finetuning_all.py
@@ -97,7 +97,7 @@ for cv_num in cv_range:
         logging_steps=1,
         per_device_train_batch_size=8,
         per_device_eval_batch_size=64,
-        num_train_epochs=8,
+        num_train_epochs=6,
         weight_decay=0.01,
         load_best_model_at_end=True,
         save_total_limit=1,
