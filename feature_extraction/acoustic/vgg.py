c5a9a89861568a27a3e24a1aec344584f2de30d4
annafavaro <anna.favaro96@gmail.com>
2024-02-26 10:12:35 -0500

    .

--- feature_extraction/acoustic/vgg.py
+++ feature_extraction/acoustic/vgg.py
@@ -1,7 +1,9 @@
+# conda activate vggish
 import torch
 import os
 import sys
 from numpy import save
+import numpy as np
 
 if __name__ == "__main__":
 
@@ -18,6 +20,8 @@ if __name__ == "__main__":
         features_vggish = model_vggish.forward(audio_file)
         features_vggish = features_vggish.cpu()
         features_vggish_np = features_vggish.detach().numpy()
+        features_vggish_np = np.mean(features_vggish_np, axis=0)
+        features_vggish_np = features_vggish_np.reshape(1, -1)
         print(features_vggish_np.shape)
         print(type(features_vggish_np))
         print(output_dir + base + '.npy')
