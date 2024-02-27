b0ef43bb9c8b69d87d41be01d0fa7b89432e1255
annafavaro <anna.favaro96@gmail.com>
2024-02-27 15:06:01 -0500

    .

--- feature_extraction/linguistic/word_similairty.py
+++ feature_extraction/linguistic/word_similairty.py
@@ -7,14 +7,22 @@ from gensim.models import KeyedVectors
 words = ['CookieJar', 'Curtain', 'Cupboard', 'Dishware', 'Dishcloth', 'Window', 'Water',
          'Stool', 'Girl', 'Mother', 'Outside', 'Boy', 'Cookie']
 
-combinations = list(itertools.combinations(words, 2))
-pair_lists = [[pair[0], pair[1]] for pair in combinations]
 #model = api.load("word2vec-google-news-300")
 #Word2Vec.load("word2vec.model")
 #word_vectors = model.wv
 #v_mango = model['mango']
 #print(v_mango)
 #print('compute similarity')
-#
+
+combinations = list(itertools.combinations(words, 2))
+
+# Include combinations of each word with itself
+for word in words:
+    combinations.append((word, word))
+
+# Store combinations as lists
+pair_lists = [[pair[0], pair[1]] for pair in combinations]
+
+# Print all combinations
 for pair_list in pair_lists:
     print(pair_list)
