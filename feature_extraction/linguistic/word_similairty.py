5fd65200669ab6c79a6c154d387137ae5c8df37f
annafavaro <anna.favaro96@gmail.com>
2024-02-27 15:02:50 -0500

    .

--- feature_extraction/linguistic/word_similairty.py
+++ feature_extraction/linguistic/word_similairty.py
@@ -11,9 +11,9 @@ combinations = list(itertools.combinations(words, 2))
 pair_lists = [[pair[0], pair[1]] for pair in combinations]
 model = api.load("word2vec-google-news-300")
 #Word2Vec.load("word2vec.model")
-word_vectors = model.wv
-v_mango = word_vectors['mango']
-pritn(v_mango)
+#word_vectors = model.wv
+v_mango = model['mango']
+print(v_mango)
 #print('compute similarity')
 #
 #for pair_list in pair_lists:
